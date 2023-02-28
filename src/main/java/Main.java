949589533e3c37064497250ccae99447dac221f6
aQuere <ddenisshelmanov@gmail.com>
2023-02-28 13:12:12 +0300

    temp c

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,22 @@
+import java.net.InetSocketAddress;
 import java.time.LocalDate;
+import com.sun.net.httpserver.HttpServer;
 
 public class Main {
     public static void main(String[] args) {
-        ContactDao contactDao = new ContactDao();
-        Contact contact = new Contact(
-                "Denis",
+        // ContactDao contactDao = new ContactDao();
+        // Contact contact = new Contact(
+        //         "Denis",
+        //
+        //         "Hormozi",
+        //         "9991283418",
+        //         LocalDate.of(2000, 10, 14)
+        // );
+        // // contactDao.save(contact);
+        //
+        // contactDao.closeConnection();
 
-                "Hormozi",
-                "9991283418",
-                LocalDate.of(2000, 10, 14)
-        );
-        // contactDao.save(contact);
-
-        contactDao.closeConnection();
+        // HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        // server.createContext()
     }
 }
